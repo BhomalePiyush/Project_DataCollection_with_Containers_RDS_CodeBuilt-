830a441276f4db50bd37044161ebf03c0169065b
Piyush <piyushbhomale1999@gmail.com>
2022-03-27 14:16:42 +0530

    builtspec.yaml test

--- ecs-devops-cdk/app.py
+++ ecs-devops-cdk/app.py
@@ -9,7 +9,7 @@ from Stacks.ECS_Cluster import ECS
 dev_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
 
 app = cdk.App()
-#database = Database(app, "databasestack", env=dev_env)
+database = Database(app, "databasestack", env=dev_env)
 genrator = ECS(app, "datagenrator", env=dev_env)
-#genrator.add_dependency(database, "database is required to put records")
+genrator.add_dependency(database, "database is required to put records")
 app.synth()
